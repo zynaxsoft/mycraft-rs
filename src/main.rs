a6447128dbc7b8f26f30a9962bdefc3776b67b30
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2020-08-19 18:17:04 +0900

    add enum for protocol state and add client struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use async_std::{io::BufWriter, net::TcpListener, net::TcpStream, prelude::*, task};
 
-use mycraft::packet::{builder::PacketBuilder, codec::{McCodec, Framed}, reader::McBytesReader};
+use mycraft::packet::{
+    builder::PacketBuilder,
+    codec::{Framed, McCodec},
+    reader::McBytesReader,
+};
 
 #[async_std::main]
 async fn main() {
@@ -14,44 +18,67 @@ async fn main() {
     }
 }
 
+#[derive(Debug, Copy, Clone)]
+pub enum ProtocolState {
+    Handshaking,
+    Status,
+    Login,
+    Play,
+}
+
+struct Client {
+    state: ProtocolState,
+}
+
+impl Client {
+    pub fn new() -> Self {
+        Self {
+            state: ProtocolState::Handshaking,
+        }
+    }
+}
+
+impl Default for Client {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 async fn client_loop(stream: TcpStream) {
     let mut framed = Framed::new(&stream, McCodec);
     let mut writer = BufWriter::new(&stream);
-    let mut state: u32 = 0;
+    let mut client = Client::new();
     while let Some(frame) = framed.next().await.transpose().unwrap() {
-        dispatch(frame, &mut writer, &mut state).await;
+        dispatch(frame, &mut writer, &mut client).await;
     }
     drop(stream);
 }
 
-async fn dispatch(data: Vec<u8>, writer: &mut BufWriter<&TcpStream>, state: &mut u32) {
+async fn dispatch(data: Vec<u8>, writer: &mut BufWriter<&TcpStream>, client: &mut Client) {
     let mut reader = McBytesReader::from_vec(data);
     let packet_id = reader.read_varint().unwrap();
-    match state {
-        // Handshaking
-        0 => match packet_id {
+    match client.state {
+        ProtocolState::Handshaking => match packet_id {
             0x00 => {
                 handshake(&mut reader).await;
-                *state = 2;
+                client.state = ProtocolState::Login;
             }
             _ => println!("Got unsupported packet id: {:x}", packet_id),
         },
-        // Login
-        2 => match packet_id {
+        ProtocolState::Login => match packet_id {
             0x00 => {
                 login_start(&mut reader, writer).await;
-                *state = 3;
+                client.state = ProtocolState::Play;
             }
             _ => println!("Got unsupported packet id: {:x}", packet_id),
         },
-        // Play
-        3 => match packet_id {
-            0x00 => {
-                println!("got 0 on play");
+        ProtocolState::Play => match packet_id {
+            id @ 0x00..=0xFF => {
+                println!("got {} on play", id);
             }
             _ => println!("Got unsupported packet id: {:x}", packet_id),
         },
-        _ => println!("Unsupported state {}", state),
+        _ => println!("Unsupported state {:?}", client.state),
     }
 }
 
